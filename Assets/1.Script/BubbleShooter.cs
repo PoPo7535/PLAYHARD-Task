fce8a79b6bfb0b86cef29f4658e2526f0746d3bc
PoPo <dltngus7535@gmail.com>
2025-11-14 23:34:12 +0900

    슈터 좌표계산

--- Assets/1.Script/BubbleShooter.cs
+++ Assets/1.Script/BubbleShooter.cs
@@ -1,6 +1,11 @@
 using System;
+using System.Numerics;
+using Sirenix.OdinInspector;
+using Sirenix.Utilities;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class BubbleShooter : MonoBehaviour
 {
@@ -8,6 +13,45 @@ public class BubbleShooter : MonoBehaviour
     public float viewAngle = 90f;
     private int segments = 5;         // 라인 분해도
 
+    public GameObject[] test;
+    
+    [Button]
+    public RaycastHit2D[] Shooter()
+    {
+        var result = new RaycastHit2D[2];
+
+        var screenPos = GetPointerWorldPosition();
+        var dir = (screenPos - (Vector2)transform.position).normalized;
+        
+        // 각도확인
+        if (Vector3.Angle(transform.forward, dir) < viewAngle / 2f)
+            return null;
+
+        result[0] = Physics2D.Raycast(transform.position, dir, viewDis);
+        if (result[0].transform.CompareTag("Wall"))
+        {
+            var point = result[0].point - (dir * 0.01f);
+            dir = Vector3.Reflect(dir, result[0].normal);
+            result[1] = Physics2D.Raycast(point, dir, viewDis);
+        }
+        test[0].transform.position = result[0].point;
+        test[1].transform.position = result[1].point;
+        return result;
+    }
+
+    private Vector2 GetPointerWorldPosition()
+    {
+        Vector2 screenPos;
+
+        // PC 마우스
+        if (Input.touchCount == 0)
+            screenPos = Input.mousePosition;
+        // 모바일 터치
+        else
+            screenPos = Input.GetTouch(0).position;
+
+        return Camera.main.ScreenToWorldPoint(screenPos);
+    }
     
 #if UNITY_EDITOR
     public bool showGizmos = true;
