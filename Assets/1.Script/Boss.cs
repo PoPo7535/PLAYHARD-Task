32f142750e9406d0a384d6f446df0b406f6c9088
PoPo <dltngus7535@gmail.com>
2025-11-17 01:10:15 +0900

    버그픽스 및 배열추가

--- Assets/1.Script/Boss.cs
+++ Assets/1.Script/Boss.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-    private Vector2Int[] _leftLine =
+    private readonly Vector2Int[] _leftLine =
     {
-        new(2, 4), new(1, 4),
+        new(3, 4), new(2, 4), new(1, 4),
         new(0, 5),
         new(1, 6), new(2, 6), new(3, 6), new(4, 6),
         new(4, 7),
@@ -14,27 +14,41 @@ public class Boss : MonoBehaviour
         new(0, 9),
         new(1, 10), new(2, 10), new(3, 10)
     };
-    [Button]
-    public void Foo()
+    private readonly Vector2Int[] _rightLine =
     {
-        HexagonGrid.I.SetBubble(null, _leftLine[0]);
-        for (int i = _leftLine.Length - 2; i >= 0; i--) 
+        new(7, 4), new(8, 4), new(9, 4),
+        new(9, 5),
+        new(9, 6), new(8, 6), new(7, 6), new(6, 6),
+        new(5, 7),
+        new(6, 8), new(7, 8), new(8, 8), new(9, 8),
+        new(9, 9),
+        new(9, 10), new(8, 10), new(7, 10)
+    };
+
+    [Button]
+    public void Test()
+    {
+        BubbleLineRefill(_leftLine);
+    }
+    [Button]
+    public void Test2()
+    {
+        BubbleLineRefill(_rightLine);
+    }
+
+    public void BubbleLineRefill(Vector2Int[] line)
+    {
+        if (HexagonGrid.I.IsValid(line.Last()))
+            return;
+        HexagonGrid.I.SetBubble(null, line[0]);
+        for (int i = line.Length - 2; i >= 0; i--)
         {
-            if (HexagonGrid.I.IsValid(_leftLine[i]))
-            {
-                if (i != 0)
-                    HexagonGrid.I.MoveCellBubble(_leftLine[i], _leftLine[i + 1]);
-                else
-                    HexagonGrid.I.MoveCellBubble(_leftLine[i], _leftLine[i + 1], () =>
-                    {
-                        if(false == HexagonGrid.I.IsValid(_leftLine.Last()))
-                            Foo();
-                    });
-            }
-        }
-        for (int i = 1; i < _leftLine.Length; ++i)
-        {
-            
+            if (false == HexagonGrid.I.IsValid(line[i])) 
+                continue;
+            if (i != 0)
+                HexagonGrid.I.MoveCellBubble(line[i], line[i + 1]);
+            else
+                HexagonGrid.I.MoveCellBubble(line[i], line[i + 1], () => BubbleLineRefill(line));
         }
     }
 }
