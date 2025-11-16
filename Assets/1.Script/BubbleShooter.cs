2197feb663d7fe509d1f89cb7d3d40c6a2b96def
PoPo <dltngus7535@gmail.com>
2025-11-16 15:46:19 +0900

    시야각 버그픽스 및 예측

--- Assets/1.Script/BubbleShooter.cs
+++ Assets/1.Script/BubbleShooter.cs
@@ -2,6 +2,7 @@ using System;
 using System.Numerics;
 using Sirenix.OdinInspector;
 using Sirenix.Utilities;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Serialization;
 using Vector2 = UnityEngine.Vector2;
@@ -11,35 +12,58 @@ public class BubbleShooter : MonoBehaviour
 {
     public LineParticle lineParticle;
     public HexagonGrid grid;
-    private int _segments = 5;  
     public float viewDis = 10f;
     public float viewAngle = 90f;
+    private int _segments = 5;  
 
+    private Bubble _predictionBubble;
+
+    public void Start()
+    {
+        _predictionBubble = BubblePool.I.Pool.Get();
+        _predictionBubble.tag = "Untagged";
+        _predictionBubble.gameObject.SetActive(false);
+        _predictionBubble.SetType(BubbleType.Bule);
+        _predictionBubble.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 50);
+        _predictionBubble.GetComponent<CircleCollider2D>().enabled = false;
+    }
 
     public void Update()
     {
+        if (Input.GetMouseButtonUp(0) ||
+            Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            lineParticle.gameObject.SetActive(false);
+            _predictionBubble.gameObject.SetActive(false);
+        }
+
         if (Input.GetMouseButtonDown(0) ||
             Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
+            lineParticle.gameObject.SetActive(true);
+            _predictionBubble.gameObject.SetActive(true);
+        }
+        
+        if (Input.GetMouseButton(0) ||
+            Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
             var hit = Shooter();
+            if (hit.IsUnityNull())
+                return;
             if (hit[1].transform.CompareTag("Bubble"))
             {
-                // target.transform.position = grid.GetGridPosition(grid.GetHitPointToCell(hit[1]));
+                _predictionBubble.transform.position = grid.GetGridPosition(grid.GetHitPointToCell(hit[1]));
             }
         }
-        // 모바일 터치 시작
-
     }
 
-    [Button]
-    public RaycastHit2D[] Shooter()
+    private RaycastHit2D[] Shooter()
     {
         var result = new RaycastHit2D[2];
 
         var screenPos = GetPointerWorldPosition();
         var dir = (screenPos - (Vector2)transform.position).normalized;
-        
-        if (Vector3.Angle(transform.forward, dir) < viewAngle / 2f)
+        if (viewAngle / 2f < Vector2.Angle(transform.up, dir))
             return null;
 
         result[1] = result[0] = Physics2D.CircleCast(transform.position, 0.07f, dir, viewDis);
@@ -53,11 +77,6 @@ public class BubbleShooter : MonoBehaviour
         lineParticle.SetPosition(1, result[0].point);
         lineParticle.SetPosition(2, result[1].point);
         return result;
-    }
-
-    private void DrawLine()
-    {
-        
     }
 
     private Vector2 GetPointerWorldPosition()
